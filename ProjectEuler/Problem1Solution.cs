b5327916e0a0d3550f6117a2cde5d4f3e3653637
Matthew Rycraft <mattrycraft@comcast.net>
2020-04-07 20:34:15 -0500

    Solution to problem 1 in C#

---
+++ ProjectEuler/Problem1Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    class Problem1Solution
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for(int i = 1; i < 1000; i++)
+            {
+                bool check = isMultiple(i);
+                
+                
+                if (check)
+                {
+                    sum += i;
+                }
+            
+            }
+
+            Console.WriteLine(sum);
+
+
+        }
+
+        static bool isMultiple(int currentIndex)
+        {
+            bool check = currentIndex % 3 == 0 || currentIndex % 5 == 0;
+            return check; 
+        }
+    }
+}
